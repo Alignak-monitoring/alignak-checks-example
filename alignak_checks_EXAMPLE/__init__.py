9274d86cae69111d73991fe0488801f19186d672
Frédéric MOHIER <frederic.mohier@gmail.com>
2015-11-22 11:15:01 +0000

    Version 0.2.2

--- alignak_checks_EXAMPLE/__init__.py
+++ alignak_checks_EXAMPLE/__init__.py
@@ -15,7 +15,7 @@
 __checks_type__ = "EXAMPLE"
 
 # Application manifest
-__version__ = u"0.2.1"
+__version__ = u"0.2.2"
 __author__ = u"Frédéric MOHIER"
 __author_email__ = "frederic.mohier@gmail.com"
 __copyright__ = u"(c) 2015 - %s" % __author__
